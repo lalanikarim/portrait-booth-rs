b7d52a6b9c405751692c81a4fb5a32c99fe68bba
Karim Lalani <jimmy00784@gmail.com>
2023-06-21 11:31:46 -0500

    reorganized home page

--- src/components/home_page.rs
+++ src/components/home_page.rs
@@ -15,6 +15,13 @@ pub enum HomePageResponse {
     NotLoggedIn,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, PartialOrd)]
+pub enum ActiveView {
+    Login,
+    LoginOtp,
+    Signup,
+}
+
 #[server(HomePageRequest, "/api")]
 pub async fn home_page_request(cx: Scope) -> Result<HomePageResponse, ServerFnError> {
     let auth = crate::auth::auth(cx).expect("Auth should be present");
@@ -29,6 +36,7 @@ pub async fn home_page_request(cx: Scope) -> Result<HomePageResponse, ServerFnEr
 #[component]
 pub fn HomePage(cx: Scope) -> impl IntoView {
     let completed = create_action(cx, |()| async {});
+    let (active_view, set_active_view) = create_signal(cx, ActiveView::Login);
     let home_page_resource = create_resource(
         cx,
         move || (completed.version().get(),),
@@ -39,41 +47,82 @@ pub fn HomePage(cx: Scope) -> impl IntoView {
         <Suspense fallback=move || {
             view! { cx, <div>"Loading..."</div> }
         }>
-            {
-                move || {
-                    let response = home_page_resource.read(cx);
-                    match response {
-                        None => {
-                            view! { cx, <div>"Loading..."</div> }
-                                .into_view(cx)
-                        }
-                        Some(response) => {
-                            match response {
-                                Err(e) => {
-                                    view!{cx, <div class="error">"Error: "{e.to_string()}</div>}.into_view(cx)
+            {move || {
+                let response = home_page_resource.read(cx);
+                match response {
+                    None => {
+                        view! { cx, <div>"Loading..."</div> }
+                            .into_view(cx)
+                    }
+                    Some(response) => {
+                        match response {
+                            Err(e) => {
+                                view! { cx, <div class="error">"Error: " {e.to_string()}</div> }
+                                    .into_view(cx)
+                            }
+                            Ok(HomePageResponse::LoggedIn(user)) => {
+                                view! { cx,
+                                    <div>"Logged in: " {user.name}</div>
+                                    <Logout completed=completed/>
+                                    <OrdersView/>
                                 }
-                                Ok(HomePageResponse::LoggedIn(user)) => {
-                                    view! { cx,
-                                        <div>"Logged in: " {user.name}</div>
-                                        <Logout completed=completed/>
-                                        <OrdersView/>
-                                    }
-                                        .into_view(cx)
+                                    .into_view(cx)
+                            }
+                            Ok(HomePageResponse::NotLoggedIn) => {
+                                view! { cx,
+                                    <div>"Not Logged In Response"</div>
+                                    <div class="container">
+                                        <div class="flex flex-row justify-between">
+                                            <div style:display=move || if active_view.get() == ActiveView::Login { "none" } else { "inline-block" }>
+                                                <button
+                                                    on:click=move |_| {
+                                                        set_active_view.update(|v| *v = ActiveView::Login);
+                                                    }
+                                                    class="green w-full"
+                                                >
+                                                    "Login with Password"
+                                                </button>
+                                            </div>
+                                            <div style:display=move || if active_view.get() == ActiveView::LoginOtp { "none" } else { "inline-block" }>
+                                                <button
+                                                    on:click=move |_| {
+                                                        set_active_view.update(|v| *v = ActiveView::LoginOtp);
+                                                    }
+                                                    class="green w-full"
+                                                >
+                                                    "Login with Code"
+                                                </button>
+                                            </div>
+                                            <div style:display=move || if active_view.get() == ActiveView::Signup { "none" } else { "inline-block" }>
+                                                <button
+                                                    on:click=move |_| {
+                                                        set_active_view.update(|v| *v = ActiveView::Signup);
+                                                    }
+                                                    class="green w-full"
+                                                >
+                                                    "Signup"
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {move || match active_view.get() {
+                                        ActiveView::Login => {
+                                            view! { cx, <Login completed/> }
+                                        }
+                                        ActiveView::LoginOtp => {
+                                            view! { cx, <LoginOtp completed/> }
+                                        }
+                                        ActiveView::Signup => {
+                                            view! { cx, <Signup completed/> }
+                                        }
+                                    }}
                                 }
-                                Ok(HomePageResponse::NotLoggedIn) => {
-                                    view! { cx,
-                                        <div>"Not Logged In Response"</div>
-                                        <Login completed=completed/>
-                                        <LoginOtp completed=completed/>
-                                        <Signup completed=completed/>
-                                    }
-                                        .into_view(cx)
-                                }
+                                    .into_view(cx)
                             }
                         }
                     }
                 }
-            }
+            }}
         </Suspense>
     }
 }
