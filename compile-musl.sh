8e51820a4f1b7399895febe054cfc25c955ed01f
Karim Lalani <jimmy00784@gmail.com>
2023-06-27 23:31:33 -0500

    fix for first run

--- compile-musl.sh
+++ compile-musl.sh
@@ -2,7 +2,7 @@
 set -e
 cargo clean
 TARGET_CC=x86_64-linux-musl-gcc LEPTOS_BIN_TARGET_TRIPLE=x86_64-unknown-linux-musl SQLX_OFFLINE=true cargo leptos build -r
-rm -r deploy/*
+rm -f -r deploy/*
 mkdir deploy/app
 cp target/server/x86_64-unknown-linux-musl/release/portrait-booth deploy/app/
 cp -r target/site deploy/app/
